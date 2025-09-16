c08b5f50495ca2a618554cbe5c53852698b7e3fc
986168276 <986168276@qq.com>
2025-09-16 21:23:34 +0800

    实现物品栏之间的物品交换；实现当鼠标悬停在物品栏中的物品上时出现注释说明。1.添加slotNumber用于标记插槽的序列号并编写拖拽过程（UIInventorySlot）2.让物品互换位置（InventoryManager）3.更新Inspector中的slotNumber值。详情：https://blog.csdn.net/benben044/article/details/144995734?spm=1001.2101.3001.10752 1.制作预制件InventoryTextBox，添加ContentSizeFitter、Vertical Layout Group组件并设置 2.添加2个子元素Image对象并为其添加Vertical Layout Group组件并设置 3.创建UIInventoryTextBox脚本 4.为以后设置工具的description描述信息并增加获取道具类型描述的功能（Settings，InventoryManager）5.增加属性，用于记录Bar对应的弹出框（UIInventoryBar）6.编写悬停时出现注释的代码及以一些额外需要注意的事项（UIInventorySlot）7.将UIInventorySlot0~11添加InventoryTextBox属性。详情：https://blog.csdn.net/benben044/article/details/145074826?spm=1001.2101.3001.10796

--- Assets/Scrpits/Misc/Settings.cs
+++ Assets/Scrpits/Misc/Settings.cs
@@ -49,6 +49,14 @@ public static class Settings
     public static int idleRight;
 
 
+    //Tool
+    public const string HoeingTool = "Hoe"; 
+    public const string ChoppingTool = "Axe"; 
+    public const string BreakingTool = "Pickaxe"; 
+    public const string ReapingTool = "Scythe";
+    public const string WateringTool = "Watering Can";
+    public const string CollectingTool = "Basket";
+
     //static constructor¾²Ì¬¹¹Ôìº¯Êý
     static Settings()
     {
