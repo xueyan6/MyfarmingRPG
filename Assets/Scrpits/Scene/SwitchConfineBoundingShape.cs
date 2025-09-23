45e887bfe008479f917eacf9cfe00802fca8aa96
986168276 <986168276@qq.com>
2025-09-23 17:14:32 +0800

    实现场景切换。1.编写切换场景区域的脚本（SceneTeleport）。2.在场景1中设置SceneTeleport的Collider大小，并拖拽到场景1路口。 3.在场景2中获得需要player刷新的坐标点，并填写在场景1的SceneTeleport中。 4.在场景2中SceneTeleport的Collider大小，并拖拽到场景2路口。 5.在场景1中获得需要player刷新的坐标点，并填写在场景2的SceneTeleport中。详情：https://blog.csdn.net/benben044/article/details/145538857?spm=1001.2101.3001.10752 创建屋内场景并实现屋内外场景切换。详情：https://blog.csdn.net/benben044/article/details/145582625?spm=1001.2101.3001.10796

--- Assets/Scrpits/Scene/SwitchConfineBoundingShape.cs
+++ Assets/Scrpits/Scene/SwitchConfineBoundingShape.cs
@@ -18,12 +18,13 @@ public class SwitchConfineBoundingShape : MonoBehaviour
 
 
     /// <summary>
-    /// Switch the collider that cinemachine uses to define the edges of the screen
+    /// Switch the collider that cinemachine uses to define the edges of the screen切换Cinemachine用于定义屏幕边缘的碰撞体
     /// </summary>
-    
+
     private void SwitchBoundingShape()
     {
         //Get the polygon collider on the "boundsconfiner" gameobject which is used by cinemachine to prevent the camera going beyond the screen edges
+        //获取用于防止摄像机超出屏幕边缘的“boundsconfiner”游戏对象上的多边形碰撞体（该对象由Cinemachine使用）
         PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
 
         CinemachineConfiner cinemachineconfiner=GetComponent<CinemachineConfiner>();
@@ -31,6 +32,7 @@ public class SwitchConfineBoundingShape : MonoBehaviour
         cinemachineconfiner.m_BoundingShape2D = polygonCollider2D;
 
         //since the confiner bounds have changed need to call this to clear the cache
+        //由于限制器边界已变更，需调用此方法清除缓存
 
         cinemachineconfiner.InvalidatePathCache();
     }
