fa815c78e121d1a6f28a18caa8aa9c8c28ee04f7
986168276 <986168276@qq.com>
2025-10-01 18:39:58 +0800

    实现为预制体创建池管理器，允许一个预制对象池被创建和重用。1.创建PoolManager.cs脚本。详情：https://blog.csdn.net/benben044/article/details/147807054?spm=1001.2101.3001.10796 实现割草时有粒子特效出现。1.配置粒子特效。2.制作Reaping粒子特效预制体。3.增加触发粒子特效事件（EventHandler）4.增加粒子特效枚举（Enums）5.创建VFXManager脚本。6.增加触发粒子特效代码（Player）7.在Unity创建VFXManager空对象，并将Reaping预制体拖入其中。8.设置PoolManager。详情https://blog.csdn.net/benben044/article/details/147873798?spm=1001.2101.3001.10796

---
+++ Assets/Scrpits/VFX/VFXManager.cs
@@ -0,0 +1,50 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class VFXManager : SingletonMonobehaviour<VFXManager>
+{
+    private WaitForSeconds twoSeconds;// 2秒等待计时器
+    [SerializeField] private GameObject reapingPrefab = null;// 收割特效预制体
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        twoSeconds = new WaitForSeconds(2f);// 初始化计时器
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.HarvestActionEffectEvent -= displayHarvestActionEffect;
+    }
+
+    private void OnEnable()
+    {
+        EventHandler.HarvestActionEffectEvent += displayHarvestActionEffect;
+    }
+
+    private IEnumerator DisableHarvestActionEffect(GameObject effectGameObject, WaitForSeconds secondsToWait)// 协程方法：延迟关闭特效对象
+    {
+        yield return secondsToWait;// 等待指定时长
+        effectGameObject.SetActive(false);// 禁用特效游戏对象
+    }
+
+    private void displayHarvestActionEffect(Vector3 effectPosition, HarvestActionEffect harvestActionEffect) // 处理特效显示的主方法
+    {
+        switch (harvestActionEffect)// 根据特效类型分支处理
+        {
+            case HarvestActionEffect.reaping:// 收割特效
+                GameObject reaping = PoolManager.Instance.ReuseObject(reapingPrefab, effectPosition, Quaternion.identity); // 从对象池获取特效实例
+                reaping.SetActive(true);// 激活特效对象
+                StartCoroutine(DisableHarvestActionEffect(reaping, twoSeconds)); // 启动协程2秒后自动关闭
+                break;
+
+            case HarvestActionEffect.none:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
