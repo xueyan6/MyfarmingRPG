992b7dc89d5d42dc381b1902059756502674a367
986168276 <986168276@qq.com>
2025-11-18 18:00:19 +0800

    实现打包游戏文件并设置退出游戏按钮。1.将UI对象中PauseMenuPanel对象下的Tab1改名Tab1QuitGame，添加Vertical Layout Group组件并设置。2.在Tab1QuitGame下添加子物体命名为QuitGameButton，添加Image和Button组件并设置。3.在QuitGameButton下Create -> UI -> Text - TextMeshPro并设置。4.修改SelectionTabButtonPanel下第2个SelectionButton的Text的值为Quit Game。5.添加退出游戏的方法（UIManager）6.回到Unity，在QuitGameButton的Button组件中+一次，并拖入UI组件，选择QuitGame方法。7.点开 Build Settings点击"Add Open Scenes"。8.配置Player Settings。9.添加#if和#endif代码（TilemapGridProperties）10.添加自动全屏进行游戏的方法（GameManager）11.回到Unity，在默认场景添加新对象GameManager，并添加GameManager脚本。12.点开Build Settings并Build。13.点开生成的游戏文件并测试。 详情：https://blog.csdn.net/benben044/article/details/148255633?spm=1001.2101.3001.10796

--- Assets/Scrpits/Inventory/InventoryManager.cs
+++ Assets/Scrpits/Inventory/InventoryManager.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.Progress;
+
 
 public class InventoryManager : SingletonMonobehaviour<InventoryManager>
 {
--- Assets/Scrpits/Item/ItemNudge.cs
+++ Assets/Scrpits/Item/ItemNudge.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class ItemNudge : MonoBehaviour
